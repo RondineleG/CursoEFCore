ce50a9d2f4f66357cba305fd8275a3945e766b75
Rondineleg <rondineleg@edu.univali.br>
2020-09-04 18:06:35 -0300

    Criando classe de  contexto  de banco dados

---
+++ src/CursoEFCore/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+using CursoEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace CursoEFCore.Data
+{
+    public class AppDbContext : DbContext
+    {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p=>p.AddConsole());
+
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer("Data source=(localdb)\\mssqllocaldb;Initial Catalog=CursoEFCore;Integrated Security=true",
+                 p=> p.EnableRetryOnFailure(
+                     maxRetryCount: 3, 
+                     maxRetryDelay: TimeSpan.FromSeconds(15), 
+                     errorNumbersToAdd: null).MigrationsHistoryTable("curso_ef_core"));
+        }
+    }
+}
